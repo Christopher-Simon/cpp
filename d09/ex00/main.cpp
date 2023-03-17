1579852b1af560151f9dcfa3b2f2e7e9306e8a9b
Christopher-Simon <christopher.simon.wells@gmail.com>
2023-03-17 18:42:49 +0100

    todo, 00

--- d09/ex00/main.cpp
+++ d09/ex00/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 int	main(void)
 {
 	std::ifstream	ifs;
 	std::string		line;
+	std::map<std::string, std::string>		db;
 
 	ifs.open("data.csv");
 	if (ifs.fail())
 		return (std::cerr << "Error file opening" << std::endl, 
 		1);
-	std::getline(ifs, line);
-	if (!ifs.eof() && ifs.fail())
-		return (std::cerr << "Error getline" << std::endl,
-		ifs.close(),
-		1);
-	std::cout << line << std::endl;
+	for (; !ifs.eof(); )
+	{
+		std::getline(ifs, line, ',');
+		std::cout << line << std::endl;
+		if (!ifs.eof() && ifs.fail())
+			return (std::cerr << "Error getline" << std::endl,
+			ifs.close(),
+			1);
+	}
 	return (0);
 }
